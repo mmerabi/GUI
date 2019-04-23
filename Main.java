9dcccfa5b7bbd1f217532ecadc4541d27d232ee8
mmerabi <merabimichael@gmail.com>
2019-04-23 12:01:27 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        home.home();
+    home.frontpage();
+
     }
 
 
--- home.java
+++ home.java
@@ -2,14 +2,24 @@ import javax.swing.*;
 
 public class home {
 
-   public static void home(){
-       JFrame gui = new JFrame();
+   home(){ //constructor for the baseframe to be used on all pages
+       JFrame gui = new JFrame("Project Management System");
        gui.setVisible(true);
        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        gui.setResizable(true);
-       gui.setSize(1000,700);
+       gui.setSize(1200,1000);
        gui.setLocationRelativeTo(null);
-       gui.setTitle("Project Management System");
+   }
+
+   public static void frontpage(){
+       home homepage = new home();
+       SpringLayout layout = new SpringLayout();
+
+       JButton redirectHome = new JButton("Home");
+       JButton redirectDeliverables = new JButton("Deliverables");
+       JButton redirectActItems = new JButton("Action Items");
+       JButton redirect2 = new JButton("Button4");
+       JButton Redirect3 = new JButton("Button5");
    }
 }
 
